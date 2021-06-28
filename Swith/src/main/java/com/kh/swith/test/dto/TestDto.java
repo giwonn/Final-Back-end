bd26d10cf315ea2773c6a65ef8af1b874e8b8ce2
YSN <greeobedi91@gmail.com>
2021-06-28 18:01:35 +0900

    주요사항: first commit backend :whale:

--- Swith/src/main/java/com/kh/swith/test/dto/TestDto.java
+++ Swith/src/main/java/com/kh/swith/test/dto/TestDto.java
@@ -1,6 +1,12 @@
 package com.kh.swith.test.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class TestDto {
 
 	private int empno;
