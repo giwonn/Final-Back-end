b95821b19dd1fe700a9098d79502f326bb0f03bf
Giwon Lim <l4279625@gmail.com>
2021-06-28 17:02:40 +0900

    어노테이션 Controller -> RestController로 바꿈

--- Swith/src/main/java/com/kh/swith/controller/PaymentController.java
+++ Swith/src/main/java/com/kh/swith/controller/PaymentController.java
@@ -7,18 +7,19 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.swith.biz.PaymentBiz;
 import com.kh.swith.dto.PaymentDto;
 import com.kh.swith.test.controller.TestController;
 
 /* 결제 기능 */
-@Controller("/payment")
+@RestController
+@RequestMapping(path="/payment")
 public class PaymentController {
 
 	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
