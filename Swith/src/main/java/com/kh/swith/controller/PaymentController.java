257d545dc9ef9ff6dcf7c00ef33e41bb9cb080cf
Giwon Lim <l4279625@gmail.com>
2021-06-28 18:29:22 +0900

    PaymentController logger 추가 :star:

--- Swith/src/main/java/com/kh/swith/controller/PaymentController.java
+++ Swith/src/main/java/com/kh/swith/controller/PaymentController.java
@@ -27,35 +27,40 @@ public class PaymentController {
 	@Autowired
 	private PaymentBiz biz;
 
-	
-	@RequestMapping(value="refund.do", method = RequestMethod.POST, produces="application/json; charset=utf8")
-	@ResponseBody
-	public String refund(HttpSession session, @RequestBody String paymentid) {
-		return (biz.refund(paymentid) > 0) ? "success" : "false";
-	}
-	
-	@RequestMapping(value="selectListPeriod.do", method = RequestMethod.POST)
-	@ResponseBody
-	public List<PaymentDto> selectListPeriod(HttpSession session, @RequestBody PaymentDto dto) {
-		return new ArrayList<>(biz.selectListPeriod(dto));
-	}
-	
 	@RequestMapping(value="selectList.do", method = RequestMethod.POST)
 	@ResponseBody
 	public List<PaymentDto> selectList(HttpSession session, @RequestBody int memberid) {
+		logger.info("[PaymentController] selectList.do");
 		return new ArrayList<>(biz.selectList(memberid));
 	}
 	
 	@RequestMapping(value="selectOne.do", method = RequestMethod.POST)
 	@ResponseBody
 	public PaymentDto selectOne(HttpSession session, @RequestBody String paymentid) {
+		logger.info("[PaymentController] selectOne.do");
 		return biz.selectOne(paymentid);
 	}
 	
 	@RequestMapping(value="insert.do", method = RequestMethod.POST)
 	@ResponseBody
 	public String insert(HttpSession session, @RequestBody PaymentDto dto) {
+		logger.info("[PaymentController] insert.do");
 		return (biz.insert(dto) > 0) ? "success" : "false";
 	}
+	
+	@RequestMapping(value="refund.do", method = RequestMethod.POST, produces="application/json; charset=utf8")
+	@ResponseBody
+	public String refund(HttpSession session, @RequestBody String paymentid) {
+		logger.info("[PaymentController] refund.do");
+		return (biz.refund(paymentid) > 0) ? "success" : "false";
+	}
+	
+	@RequestMapping(value="selectListPeriod.do", method = RequestMethod.POST)
+	@ResponseBody
+	public List<PaymentDto> selectListPeriod(HttpSession session, @RequestBody PaymentDto dto) {
+		logger.info("[PaymentController] selectListPeriod.do");
+		return new ArrayList<>(biz.selectListPeriod(dto));
+	}
+
 	
 }
