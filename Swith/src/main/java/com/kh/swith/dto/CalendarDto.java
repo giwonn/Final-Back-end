444cc1cd794ace48c5281a2c5592389b49ef480f
KangCine <kwg940930@gmail.com>
2021-06-29 01:18:12 +0900

    주요사항 : calendar 수정중...

--- Swith/src/main/java/com/kh/swith/dto/CalendarDto.java
+++ Swith/src/main/java/com/kh/swith/dto/CalendarDto.java
@@ -2,6 +2,8 @@ package com.kh.swith.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +21,12 @@ public class CalendarDto {
 	private int member_id;
 	private int study_group_id;
 	private String title;
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
 	private Date start;
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
 	private Date end;
 	private String content;
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
 	private Date regdate;
 
 }
