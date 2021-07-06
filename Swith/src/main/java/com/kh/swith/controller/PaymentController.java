bb6396c20821cd7ba6c53825f1e8241666d54abf
Giwon Lim <l4279625@gmail.com>
2021-07-06 20:16:31 +0900

    주요사항 : RestController에 맞게 ResponseBody 제거해줌 :start:

--- Swith/src/main/java/com/kh/swith/controller/PaymentController.java
+++ Swith/src/main/java/com/kh/swith/controller/PaymentController.java
@@ -22,41 +22,36 @@ import com.kh.swith.test.controller.TestController;
 @RequestMapping(path="/payment")
 public class PaymentController {
 
-	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+	private static final Logger logger = LoggerFactory.getLogger(PaymentController.class);
 	
 	@Autowired
 	private PaymentBiz biz;
 
 	@RequestMapping(value="selectList.do", method = RequestMethod.POST)
-	@ResponseBody
 	public List<PaymentDto> selectList(HttpSession session, @RequestBody int memberid) {
 		logger.info("[PaymentController] selectList.do");
 		return new ArrayList<>(biz.selectList(memberid));
 	}
 	
 	@RequestMapping(value="selectOne.do", method = RequestMethod.POST)
-	@ResponseBody
 	public PaymentDto selectOne(HttpSession session, @RequestBody String paymentid) {
 		logger.info("[PaymentController] selectOne.do");
 		return biz.selectOne(paymentid);
 	}
 	
 	@RequestMapping(value="insert.do", method = RequestMethod.POST)
-	@ResponseBody
 	public String insert(HttpSession session, @RequestBody PaymentDto dto) {
 		logger.info("[PaymentController] insert.do");
 		return (biz.insert(dto) > 0) ? "success" : "false";
 	}
 	
 	@RequestMapping(value="refund.do", method = RequestMethod.POST, produces="application/json; charset=utf8")
-	@ResponseBody
 	public String refund(HttpSession session, @RequestBody String paymentid) {
 		logger.info("[PaymentController] refund.do");
 		return (biz.refund(paymentid) > 0) ? "success" : "false";
 	}
 	
 	@RequestMapping(value="selectListPeriod.do", method = RequestMethod.POST)
-	@ResponseBody
 	public List<PaymentDto> selectListPeriod(HttpSession session, @RequestBody PaymentDto dto) {
 		logger.info("[PaymentController] selectListPeriod.do");
 		return new ArrayList<>(biz.selectListPeriod(dto));
