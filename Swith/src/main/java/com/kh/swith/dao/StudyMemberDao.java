a4c77c2ad0dc1b0b6ceb0c44eb5a2e4110ab9660
Changhee Park <arrans9591@gmail.com>
2021-07-12 15:05:06 +0900

    주요사항 :changhee 커밋

--- Swith/src/main/java/com/kh/swith/dao/StudyMemberDao.java
+++ Swith/src/main/java/com/kh/swith/dao/StudyMemberDao.java
@@ -8,5 +8,8 @@ public interface StudyMemberDao {
 	
 	public String selectRole(StudyMemberDto dto);
 	public List<StudyMemberDto> studyGetList(int studyId);
+	public int StudySignIn(StudyMemberDto dto);
+	public int StudySignOut(StudyMemberDto dto);
+	public int StudySignJoin(StudyMemberDto dto);
 
 }
