eb5176607706c59c3ea54810b82edde7b4b6265c
dhdhdht <breakboy0526@naver.com>
2021-07-12 14:25:31 +0900

    Merge remote-tracking branch 'origin/main' into Jin
    # Conflicts:
    #	Swith/target/m2e-wtp/web-resources/META-INF/MANIFEST.MF
    #	Swith/target/m2e-wtp/web-resources/META-INF/maven/com.kh/swith/pom.properties

--- Swith/src/main/java/com/kh/swith/dao/MemberDao.java
+++ Swith/src/main/java/com/kh/swith/dao/MemberDao.java
@@ -1,8 +1,13 @@
 package com.kh.swith.dao;
 
+import java.util.List;
+
 import com.kh.swith.dto.MemberDto;
 
 public interface MemberDao {
 	public String MemberSelect(String email);
 	public int MemberInsert(MemberDto memberDto);
+	
+	public List<MemberDto> profileSelect(String email);
+	public int profileUpdate(MemberDto memberDto);
 }
